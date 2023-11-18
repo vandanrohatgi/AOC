cf79caaa6e556838ad448e2c222666355ee9917b
Vandan Rohatgi <vandanrohatgi@gmail.com>
2023-11-18 11:22:02 +0530

    chore: improve challenge setup process

--- init.sh
+++ init.sh
@@ -2,11 +2,16 @@
 
 # Creates the challenge directory and golang files for me.
 
-# if [[ $# -eq 0  ||  $1 -eq "-h" ]];then
-#     echo "./init.sh <challenge URL> \n./init.sh https://adventofcode.com/2015/day/9"
-# fi
-#
-#
+if [[ -z $1 || $1 = "-h" ]];then
+    echo "./init.sh <challenge URL> \n./init.sh https://adventofcode.com/2015/day/9"
+    exit
+fi
+
+if [[ -z "${AOC_COOKIE}"  ]];then
+    echo -n "Paste the AOC cookie:"
+    read -s cookie
+    export AOC_COOKIE=$cookie
+fi
 
 IFS='/' read -A URL <<< $1
 
@@ -15,31 +20,9 @@ day=${URL[6]}
 
 dir=$year/day$day
 mkdir -p $dir
-
 cd $dir
-go mod init day$day
-touch main.go sol1.go sol2.go test.txt input.txt
 
-cat << EOF > main.go
-package main
+touch test.txt
+cp ../../main.go.template main.go
 
-func main(){
-    sol1()
-    //sol2()
-}
-EOF
-
-cat << EOF > sol1.go
-package main
-
-func sol1(){
-    
-}
-EOF
-cat << EOF > sol2.go
-package main
-
-func sol2(){
-
-}
-EOF
+curl $1/input --cookie "session=$AOC_COOKIE" -o input.txt
